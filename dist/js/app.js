74a4647dd8e576512213ad1c1562f10d7f47098e
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-09-16 01:53:13 +0900

    Refact2

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,54 +1,61 @@
-const btnAdd = document.querySelector('.js-btn-add');
-const errorMsg = document.querySelector('.js-error');
-const addToDoInput = document.querySelector('.js-todo-input');
-
+// タスクの完了・未完了を切り替えるイベントを変数へ格納
 /* eslint-disable no-unused-vars */
 const checkClickFunc = event => {
   /* eslint-disable no-unused-vars */
   if (event.target.textContent === '■') {
+    // 指定したタスクが完了状態なら未完了状態へ変更
     event.target.textContent = '□';
-    event.target.parentNode.className = 'todo-item js-todo-item';
+    event.target.parentNode.classList.toggle('js-todo-done');
   } else {
+    // 指定したタスクが未完了状態なら完了状態へ変更
     event.target.textContent = '■';
-    event.target.parentNode.className = 'todo-item js-todo-item js-todo-done';
+    event.target.parentNode.classList.toggle('js-todo-done');
   }
 };
 
+// タスク削除イベントを変数へ格納
 const deleteClickFunc = event => {
   let delEle = event.target.parentNode;
   event.target.parentNode.parentNode.removeChild(delEle);
 };
 
+// タスク名編集の完了イベントを変数へ格納
+// inputタグに値が入力される度に発生するイベント
+const taskNameEditEndFunc = event => {
+  // Shift+Enterで編集を完了するイベント
+  if (event.shiftKey && event.keyCode === 13) {
+    let inputVal = event.srcElement.value; // 入力された値を変数へ格納
+    let newSpanText = document.createElement('span'); // spanタグを新規作成して、入力された値を設定
+    newSpanText.classList.add('text');
+    newSpanText.classList.add('js-text');
+    newSpanText.textContent = inputVal; // spanタグへ入力された値を書き込み
+    newSpanText.addEventListener('click', taskNameClickFunc);
+    event.srcElement.parentNode.replaceChild(newSpanText, event.srcElement);
+  }
+};
+
+// タスク名がクリックされたらタスク名が編集できるようにするイベントを変数へ格納
 const taskNameClickFunc = event => {
-  let taskName = event.target.textContent;
-  let newInput = document.createElement('input');
+  let taskName = event.target.textContent; // 元のタスク名を変数へ格納
+  let newInput = document.createElement('input'); // inputタグを新規作成し、attrとeventを設定
   newInput.setAttribute('type', 'text');
   newInput.setAttribute('name', 'todo');
-  newInput.setAttribute('value', taskName);
-  newInput.addEventListener('keydown', event => {
-    if (event.shiftKey && event.keyCode === 13) {
-      let inputVal = event.srcElement.value;
-      let newSpanText = document.createElement('span');
-      newSpanText.classList.add('text js-text');
-      newSpanText.textContent = inputVal;
-      newSpanText.addEventListener('click', taskNameClickFunc);
-      event.srcElement.parentNode.replaceChild(newSpanText, event.srcElement);
-    }
-  });
+  newInput.setAttribute('value', taskName); // inputタグの既定値として元のタスク名を設定
+  newInput.addEventListener('keydown', taskNameEditEndFunc); // inputタグにShift+Enterが押されたら編集を完了するイベントを設定
   let parentNodeEle = event.target.parentNode;
   parentNodeEle.replaceChild(newInput, event.target);
   parentNodeEle.getElementsByTagName('input')[0].focus();
 };
 
+const btnAdd = document.querySelector('.js-btn-add');
+const errorMsg = document.querySelector('.js-error');
+const addToDoInput = document.querySelector('.js-todo-input');
 btnAdd.addEventListener('click', event => {
-  console.log(addToDoInput.value);
-
   if (addToDoInput.value === '') {
     errorMsg.className = 'error js-error js-error-on'; //追加ボタンが押されても入力値が空ならエラーを表示するだけ
   } else {
     errorMsg.className = 'error js-error'; //追加ボタンが押されて入力値有りならエラー表示を非表示にする。
     // ToDoリストへタスクを追加する処理を開始
-    let todoLists = document.querySelector('.js-todo-lists');
     let newElement = document.createElement('div');
     newElement.classList.add('todo-item');
     newElement.classList.add('js-todo-item');
@@ -66,26 +73,27 @@ btnAdd.addEventListener('click', event => {
       newElement.appendChild(newSpan);
     }
 
+    let todoLists = document.querySelector('.js-todo-lists');
     todoLists.insertBefore(newElement, todoLists.firstChild); //先頭へ追加する
 
-    addToDoInput.value = '';
+    addToDoInput.value = ''; // 追加用inputタグの入力値を空にする
   }
 });
 
 // タスク完了・未完了の切り替えイベント
-const checkClick = document.querySelectorAll('.check');
+const checkClick = document.querySelectorAll('.js-check');
 for (let i = 0; i < checkClick.length; i++) {
   checkClick[i].addEventListener('click', checkClickFunc);
 }
 
 // ゴミ箱がクリックされた場合のDOM削除イベント
-const deleteClicks = document.querySelectorAll('.delete');
+const deleteClicks = document.querySelectorAll('.js-delete');
 for (let i = 0; i < deleteClicks.length; i++) {
   deleteClicks[i].addEventListener('click', deleteClickFunc);
 }
 
 // タスク名がクリックされた場合のタスク名変更イベント
-const taskNameClicks = document.querySelectorAll('.text');
+const taskNameClicks = document.querySelectorAll('.js-text');
 for (let i = 0; i < taskNameClicks.length; i++) {
   taskNameClicks[i].addEventListener('click', taskNameClickFunc);
 }
@@ -100,9 +108,9 @@ searchEle.addEventListener('keyup', event => {
     let todoText = todoItems[i].querySelector('.text').textContent;
     if (!todoText.match(regexp)) {
       console.log(todoItems[i]);
-      todoItems[i].className = 'todo-item js-todo-item js-search-out';
+      todoItems[i].classList.add('js-search-out');
     } else {
-      todoItems[i].className = 'todo-item js-todo-item';
+      todoItems[i].classList.remove('js-search-out');
     }
   }
 });
